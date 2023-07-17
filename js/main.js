b9dbe3ded198b3248e7ec34ca1ae69dcaa8de8a0
GustavoF7 <134310930+GustavoF7@users.noreply.github.com>
2023-07-17 11:40:32 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const menu = document.querySelector('.menu');
 const cabecalhoLinks = document.querySelectorAll('.cabecalho__link');
 menuToggle.addEventListener('change', () => {
     if (menuToggle.checked) {
-        document.getElementById('icone-menu').style.backgroundImage = 'url("../img/close-menu.svg")';
+        document.getElementById('icone-menu').style.backgroundImage = 'url(./img/close-menu.svg)';
         menu.style.animation = 'menuAnimation 0.3s ease';
         menu.style.display = 'flex';
         setTimeout(() => {
@@ -13,7 +13,7 @@ menuToggle.addEventListener('change', () => {
           }, 300);
           
     } else {
-        document.getElementById('icone-menu').style.backgroundImage = 'url("../img/menu-hamburguer.svg")';
+        document.getElementById('icone-menu').style.backgroundImage = 'url(./img/menu-hamburguer.svg)';
         menu.style.animation = 'menuReverseAnimation 0.3s ease';
         cabecalhoLinks.forEach((link) => {
             link.style.opacity = '0';
